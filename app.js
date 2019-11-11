fc2b13fe11dc6983c95d9bf6945eed2d7b53cb10
FreezeByte <rosas.ruizlg@gmail.com>
2019-11-10 23:40:05 -0600

    Peticiones HTTP

--- app.js
+++ app.js
@@ -12,11 +12,26 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 //Rutas
-app.get('/test', (req, res) => {
+app.post('/test', (req, res) => {
+    //Imprimimos una peticion que le enviamos por el metodo
+    //Post, y el metodo body busca la key
+
+    //req.body 
+    //req.query
+    //req.params
+
+    console.log(req.body.Nombre);
     res.status(200).send({
-        message: "Hola mundo desde mi API de NodeJS"
-    })
-})
+        message: "<h1>Hola mundo desde mi API de NodeJS</h1>"
+    });
+});
+
+app.get('/', (req, res) => {
+    res.status(200).send(
+        "<h1>Hola mundo desde mi API de NodeJS</h1>"
+    );
+});
+
 //Exportar
 module.exports = app;
 
---
+++ models/project.js
@@ -0,0 +1,21 @@
+//Un modelo representa a un documento de la coleccion 
+//de la base de datos
+
+'use strict'
+
+//Se importa el modulo mongoose
+var mongoose = require('mongoose');
+//esquema de un modelo
+var Schema = mongoose.Schema;
+//Esquema de project, objeto molde, se pasa un objeto Json con las propiedades
+var ProjectSchema = Schema({
+    name: String,
+    descripcion: String,
+    categoria: String,
+    year: Number,
+    languages : [String]
+});
+
+module.exports = mongoose.model('Project', ProjectSchema);
+//mongoose agrega el primer parametro como coleccion, lo pone en minusculas u lo pluraliza
+//entonces: projects --> guarda los documentos en la coleccion
